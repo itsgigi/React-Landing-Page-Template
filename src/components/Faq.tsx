28aa22d2252348ecdadd30e441e05e853327643b
itsgigi <luigi.dl@hotmail.it>
2025-01-02 21:00:20 +0100

    test

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -35,7 +35,6 @@ export const Faq = () => {
       <div className="panel" ref={ref3}>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </div>
-
     </div>
   );
 }
